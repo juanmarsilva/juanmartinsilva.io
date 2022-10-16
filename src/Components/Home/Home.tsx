cb6716028b1b8cc7d21312491092ceff8a560615
Juan Martin Silva <juanmartinsilva74@gmail.com>
2022-10-16 00:13:13 -0300

    Avances en el Home

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
 import s from './Home.module.css';
+import imagen from '../../Images/Foto.jpg';
 
 const Home = () => {
     return (
-        <div>
-            <p>Hola giles</p>
+        <div className={s.container}>
+            <div className={s.principalPage}>
+                <img src={imagen} alt='imagen' className={s.img} />
+                <h1>{`< Juan Martin Silva />`}</h1>
+            </div>
+            <div className={s.principalPage}>
+                <img src={imagen} alt='imagen' className={s.img} />
+                <h1>{`< Juan Martin Silva />`}</h1>
+            </div>
         </div>
     )
 }
